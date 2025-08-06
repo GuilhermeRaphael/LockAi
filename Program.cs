01bf872181b4298031c5df9f6eb0dfea03e7df80
GuilhermeRaphael <guigaorapha07@gmail.com>
2025-08-05 21:25:11 -0300

    Ajustes em Program.cs e appsettings.json

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSomee")));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
