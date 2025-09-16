3dfbf323950fb361767ff11d18790133b9305733
GuilhermeRaphael <guigaorapha07@gmail.com>
2025-09-15 22:03:48 -0300

    adicionado cors

--- Program.cs
+++ Program.cs
@@ -3,62 +3,43 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// para serializar enums no JSON
+// Serialização de enums como string
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.MaxDepth = 64;
     });
 
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Swagger/OpenAPI
 builder.Services.AddOpenApi();
 
+// Conexão com banco de dados
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSomee")));
 
-builder.Services.AddControllers().AddJsonOptions(options =>
+// CORS
+builder.Services.AddCors(options =>
 {
-    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-    options.JsonSerializerOptions.MaxDepth = 64; // Opcional, para aumentar a profundidade máxima
+    options.AddPolicy("AllowSomee", policy =>
+        policy.WithOrigins("http://lockai.somee.com")
+              .AllowAnyMethod()
+              .AllowAnyHeader());
 });
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseRouting(); // <- necessário para mapear rotas
+app.UseCors("AllowSomee"); // <- precisa vir entre Routing e Authorization
+app.UseAuthorization(); // <- mesmo que não esteja usando autenticação, é padrão
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.MapControllers();
+app.MapControllers(); // <- mapeia os endpoints
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
