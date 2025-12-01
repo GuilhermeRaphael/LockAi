cffe8be86e9dcb9189fab5e73e191a15afe8e4b2
Gabriel Gomes <gabrielsg2000@gmail.com>
2025-11-30 23:13:03 -0300

    Update IdUsuario to use logged-in user's ID

--- Controllers/PropostaLocacaoController.cs
+++ Controllers/PropostaLocacaoController.cs
@@ -116,7 +116,7 @@ namespace LockAi.Controllers
                     Pagamento = new PropostaLocacaoPagamento
                     {
                     Data = new DateTime(2025, 9, 9),
-                    IdUsuario = 4,
+                    IdUsuario = usuarioLogado.Id,
                     DtConferencia = new DateTime(2025, 9, 10),
                     IdUsuarioConferencia = 1,
                     Situacao = SituacaoPropostaLocacaoPagamento.Aprovado,
