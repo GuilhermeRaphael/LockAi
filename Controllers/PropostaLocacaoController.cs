1b62465943e3a94a7b7e7f798a8c06e142ac8df6
Gabriel Gomes <gabrielsg2000@gmail.com>
2025-11-30 23:01:07 -0300

    Remove Comprovante value from Pagamento
    Removed the 'Comprovante' field value from the 'Pagamento' object.

--- Controllers/PropostaLocacaoController.cs
+++ Controllers/PropostaLocacaoController.cs
@@ -116,7 +116,6 @@ namespace LockAi.Controllers
                     Pagamento = new PropostaLocacaoPagamento
                     {
                     Data = new DateTime(2025, 9, 9),
-                    Comprovante = "comp.jpg",
                     IdUsuario = 1,
                     DtConferencia = new DateTime(2025, 9, 10),
                     IdUsuarioConferencia = 1,
